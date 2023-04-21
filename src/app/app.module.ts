f2c18e68bcbcb8f4b2d3fe384dd611b78de033fc
CaioCesarP <caiocesarworks@gmail.com>
2023-04-21 15:56:33 -0300

    :sparkles: feat: ajustando imagens e adicionando cabeçalho.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
+import { RodapeComponent } from './components/rodape/rodape.component';
 
 @NgModule({
-  declarations: [AppComponent, CabecalhoComponent],
+  declarations: [AppComponent, CabecalhoComponent, RodapeComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
