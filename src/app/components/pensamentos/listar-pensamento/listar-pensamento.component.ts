32eac257f6790842e2eed63bcbe1d9e20d121755
CaioCesarP <caiocesarworks@gmail.com>
2023-04-22 23:39:40 -0300

    :sparkles: feat: tratamento para quando não tiver pensamentos na lista de pensamentos.

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,10 +7,7 @@ import { Component, OnInit } from '@angular/core'
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamentos = [
-    { conteudo: 'Estou perdendo o interesse...', autoria: 'Minha mente', modelo: 'modelo2' },
-    { conteudo: 'Estou pensando sobre não ter a gente...', autoria: 'Minha mente', modelo: 'modelo1' }
-  ]
+  listaPensamentos = []
 
   constructor() { }
 
