f862fb63a3b2ca2df81b63d99077d0431ff76cf8
CaioCesarP <caiocesarworks@gmail.com>
2023-04-22 22:33:22 -0300

    :sparkles: feat: criação das rotas de criar um pensamento e listar um pensamento. Foi feito também o redirecionamento papara cada uma, completando assim a primeira parte da interação entre ambas as rotas.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component'
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component'
 import { RodapeComponent } from './components/rodape/rodape.component'
 import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component'
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { ListarPensamentoComponent } from './components/pensamentos/listar-pensamento/listar-pensamento.component'
 
 @NgModule({
-  declarations: [AppComponent, CabecalhoComponent, RodapeComponent, CriarPensamentoComponent],
+  declarations: [AppComponent, CabecalhoComponent, RodapeComponent, CriarPensamentoComponent, ListarPensamentoComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
