0510b8d064728c4a9929a014bcf205693a609c73
CaioCesarP <caiocesarworks@gmail.com>
2023-04-22 22:55:22 -0300

    :sparkles: feat: criação de card para pensamento em listagem.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { CabecalhoComponent } from './components/cabecalho/cabecalho.component'
 import { RodapeComponent } from './components/rodape/rodape.component'
 import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component'
 import { FormsModule } from '@angular/forms';
-import { ListarPensamentoComponent } from './components/pensamentos/listar-pensamento/listar-pensamento.component'
+import { ListarPensamentoComponent } from './components/pensamentos/listar-pensamento/listar-pensamento.component';
+import { PensamentoComponent } from './components/pensamentos/pensamento/pensamento.component'
 
 @NgModule({
-  declarations: [AppComponent, CabecalhoComponent, RodapeComponent, CriarPensamentoComponent, ListarPensamentoComponent],
+  declarations: [AppComponent, CabecalhoComponent, RodapeComponent, CriarPensamentoComponent, ListarPensamentoComponent, PensamentoComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
