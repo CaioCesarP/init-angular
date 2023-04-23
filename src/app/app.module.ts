64b38e8b49b1f3753ee00a80d9db95626cfc1fa2
CaioCesarP <caiocesarworks@gmail.com>
2023-04-22 21:58:15 -0300

    :sparkles: feat: Adicionando formulário para cadastro de pensamento.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
-import { RodapeComponent } from './components/rodape/rodape.component';
-import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { CabecalhoComponent } from './components/cabecalho/cabecalho.component'
+import { RodapeComponent } from './components/rodape/rodape.component'
+import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component'
+import { FormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [AppComponent, CabecalhoComponent, RodapeComponent, CriarPensamentoComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-criar-pensamento',
   templateUrl: './criar-pensamento.component.html',
-  styleUrls: ['./criar-pensamento.component.css']
+  styleUrls: ['./criar-pensamento.component.css'],
 })
 export class CriarPensamentoComponent implements OnInit {
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'dev',
+    modelo: 'modelo1',
+  };
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
+  criarPensamento = () => {
+    alert(JSON.stringify(this.pensamento))
+  };
+
+  cancelar = () => {
+    alert('Cancelado!')
+  };
 }
