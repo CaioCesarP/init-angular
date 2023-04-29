486075ea2dfa6458f253c50820f25c744b9d8018
CaioCesarP <caiocesarworks@gmail.com>
2023-04-28 23:39:04 -0300

    :construction: feat: preparando para implementação de formulário com validações.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,16 @@ import { AppComponent } from './app.component'
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component'
 import { RodapeComponent } from './components/rodape/rodape.component'
 import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component'
-import { FormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { ListarPensamentoComponent } from './components/pensamentos/listar-pensamento/listar-pensamento.component'
 import { PensamentoComponent } from './components/pensamentos/pensamento/pensamento.component'
-import { HttpClientModule } from '@angular/common/http';
-import { ExcluirPensamentoComponent } from './components/pensamentos/excluir-pensamento/excluir-pensamento.component';
+import { HttpClientModule } from '@angular/common/http'
+import { ExcluirPensamentoComponent } from './components/pensamentos/excluir-pensamento/excluir-pensamento.component'
 import { EditarPensamentoComponent } from './components/pensamentos/editar-pensamento/editar-pensamento.component'
 
 @NgModule({
   declarations: [AppComponent, CabecalhoComponent, RodapeComponent, CriarPensamentoComponent, ListarPensamentoComponent, PensamentoComponent, ExcluirPensamentoComponent, EditarPensamentoComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core'
 import { Pensamento } from '../pensamento'
 import { PensamentoService } from '../pensamento.service'
 import { Router } from '@angular/router'
+import { FormBuilder, FormGroup } from '@angular/forms'
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -9,18 +10,21 @@ import { Router } from '@angular/router'
   styleUrls: ['./criar-pensamento.component.css'],
 })
 export class CriarPensamentoComponent implements OnInit {
-  pensamento: Pensamento = {
-    conteudo: '',
-    autoria: '',
-    modelo: 'modelo1',
-  };
 
-  constructor(private router: Router, private service: PensamentoService) { }
+  formulario!: FormGroup
 
-  ngOnInit(): void { }
+  constructor(private router: Router, private formBuilder: FormBuilder, private service: PensamentoService) { }
+
+  ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      conteudo: [''],
+      autoria: [''],
+      modelo: ['modelo1'],
+    })
+  }
 
   criarPensamento = () => {
-    this.service.criar(this.pensamento).subscribe(() => this.router.navigate(['/listarPensamento']))
+    this.service.criar(this.formulario.value).subscribe(() => this.router.navigate(['/listarPensamento']))
   };
 
   cancelar = () => {
